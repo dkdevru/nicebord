b332f392667b5a300dc93c205a85ac6cec91079d
Dima Kozlov <dkdevru@gmail.com>
2018-04-18 13:02:45 +0300

    add center option

--- js/nicebord.js
+++ js/nicebord.js
@@ -21,7 +21,8 @@
             pos: 'top,right,bottom,left',
             speed : 200,
             direction: false,
-            fix: false
+            fix: false,
+            center: false
         };
         
         this.init(options);
@@ -50,46 +51,54 @@
         	var speed_in = this.options.speed;
         	var size = this.options.size;
         	var direction = this.options.direction;
+        	var center = this.options.center;
         	
         	this.$elem.children().each(function(){
         		if ($(this).attr('rel') == 'bord')
     			{
-        			
         			switch ($(this).attr('class')) 
         			{
 	        		    case 'bord_top':
 	        		    	
 	        		    	var result_top = $.grep(pos, function(e){ return e == 'top'; });
 	        		    	
-	        		    	if (result_top.length == 1 && direction === false){
+	        		    	if (result_top.length == 1 && direction === false && center === false){
 	        		    		$(this).stop().animate({opacity:'1',width:'100%'},speed_in);
-	        		    	}else if(result_top.length == 1 && direction === true){
+	        		    	}else if (result_top.length == 1 && (direction === false || direction === true) && center === true){
+	        		    		$(this).stop().animate({opacity:'1',width:'100%',left:'0%'},speed_in);
+	        		    	}else if(result_top.length == 1 && direction === true && center === false){
 	        		    		$(this).stop().animate({opacity:'1',height:size+'px'},speed_in);
 	        		    	}
 	        		        break;
 	        		    case 'bord_right':
 	        		    	var result_right = $.grep(pos, function(e){ return e == 'right'; });
-	        		    	if (result_right.length == 1 && direction === false){
+	        		    	if (result_right.length == 1 && direction === false && center === false){
 	        		    		$(this).stop().animate({opacity:'1',height:'100%'},speed_in);
-	        		    	}else if(result_right.length == 1 && direction === true){
+	        		    	}else if (result_right.length == 1 && (direction === false || direction === true) && center === true){
+	        		    		$(this).stop().animate({opacity:'1',height:'100%',top:'0%'},speed_in);
+	        		    	}else if(result_right.length == 1 && direction === true && center === false){
 	        		    		$(this).stop().animate({opacity:'1',width:size+'px'},speed_in);
 	        		    	}
 	        		    	break;
 	        		        
 	        		    case 'bord_bottom':
 	        		    	var result_bottom = $.grep(pos, function(e){ return e == 'bottom'; });
-	        		    	if (result_bottom.length == 1 && direction === false){
+	        		    	if (result_bottom.length == 1 && direction === false && center === false){
 	        		    		$(this).stop().animate({opacity:'1',width:'100%'},speed_in);
-	        		    	}else if(result_bottom.length == 1 && direction === true){
+	        		    	}else if (result_bottom.length == 1 && (direction === false || direction === true) && center === true){
+	        		    		$(this).stop().animate({opacity:'1',width:'100%',right:'0%'},speed_in);
+	        		    	}else if(result_bottom.length == 1 && direction === true && center === false){
 	        		    		$(this).stop().animate({opacity:'1',height:size+'px'},speed_in);
 	        		    	}
 	        		    	break;
 	        		        
 	        		    case 'bord_left':
 	        		    	var result_left = $.grep(pos, function(e){ return e == 'left'; });
-	        		    	if (result_left.length == 1 && direction === false){
+	        		    	if (result_left.length == 1 && direction === false && center === false){
 	        		    		$(this).stop().animate({opacity:'1',height:'100%'},speed_in);
-	        		    	}else if(result_left.length == 1 && direction === true){
+	        		    	}else if (result_left.length == 1 && (direction === false || direction === true) && center === true){
+	        		    		$(this).stop().animate({opacity:'1',height:'100%',bottom:'0%'},speed_in);
+	        		    	}else if(result_left.length == 1 && direction === true && center === false){
 	        		    		$(this).stop().animate({opacity:'1',width:size+'px'},speed_in);
 	        		    	}
 	        		    	break;
@@ -104,38 +113,48 @@
         	
         	var speed_in = this.options.speed;
         	var direction = this.options.direction;
+        	var center = this.options.center;
         	
         	this.$elem.children().each(function(){
         		if ($(this).attr('rel') == 'bord')
     			{
         			switch ($(this).attr('class')) 
         			{
-        			case 'bord_right':
-        				if (direction === false){
-        				$(this).stop().animate({opacity:'0',height:'0%'},speed_in);
-        				}else if(direction === true){
-        		    		$(this).stop().animate({opacity:'0',width:'0px'},speed_in);
-        		    	}
-        		    	break;
 	        		case 'bord_top':
-	        			if (direction === false){
+	        			if (direction === false && center === false){
         		    	$(this).stop().animate({opacity:'0',width:'0%'},speed_in);
-        				}else if(direction === true){
+        				}else if ((direction === false || direction === true) && center === true){
+        		    	$(this).stop().animate({opacity:'0',width:'0%',left:'50%'},speed_in);
+        				}else if(direction === true && center === false){
         					$(this).stop().animate({opacity:'0',height:'0px'},speed_in);
         				}
 	        		    break;
-	        		        
+	        	
+	        		case 'bord_right':
+        				if (direction === false && center === false){
+        				$(this).stop().animate({opacity:'0',height:'0%'},speed_in);
+        				}else if ((direction === false || direction === true) && center === true){
+        				$(this).stop().animate({opacity:'0',height:'0%',top:'50%'},speed_in);
+        				}else if(direction === true && center === false){
+        		    		$(this).stop().animate({opacity:'0',width:'0px'},speed_in);
+        		    	}
+        		    	break;
+	        		    
 	        		 case 'bord_bottom':
-	        			 if (direction === false){
+	        			 if (direction === false && center === false){
 	         		    	$(this).stop().animate({opacity:'0',width:'0%'},speed_in);
-	         				}else if(direction === true){
+	         				}else if ((direction === false || direction === true) && center === true){
+	         		    	$(this).stop().animate({opacity:'0',width:'0%',right:'50%'},speed_in);
+	         				}else if(direction === true && center === false){
 	         					$(this).stop().animate({opacity:'0',height:'0px'},speed_in);
 	         				}
 	        		        break;
 	        		 case 'bord_left':
-	        			 if (direction === false){
+	        			 if (direction === false && center === false){
 	         				$(this).stop().animate({opacity:'0',height:'0%'},speed_in);
-	         				}else if(direction === true){
+	         				}else if ((direction === false || direction === true) && center === true){
+	         				$(this).stop().animate({opacity:'0',height:'0%',bottom:'50%'},speed_in);
+	         				}else if(direction === true && center === false){
 	         		    		$(this).stop().animate({opacity:'0',width:'0px'},speed_in);
 	         		    	}
 	        		        break;
@@ -162,18 +181,23 @@
         	switch (this.options.orientation) 
 			{
 			case 'ckw':
-				if (this.options.direction === false){
+				if (this.options.direction === false && this.options.center === false){
         	var bord_top = '<div class="bord_top" rel="bord" style="height:' + this.options.size + 'px;left:0px;background-color:' + this.options.color + '"></div><div class="bord_right" rel="bord" style="width:' + this.options.size + 'px;top:0px;background-color:' + this.options.color + '"></div><div class="bord_bottom" rel="bord" style="height:' + this.options.size + 'px;right:0px;background-color:' + this.options.color + '"></div><div class="bord_left" rel="bord" style="width:' + this.options.size + 'px;bottom:0px;background-color:' + this.options.color + '"></div>';
-				}
+				} else if ((this.options.direction === false || this.options.direction === true) && this.options.center === true){
+		        	var bord_top = '<div class="bord_top" rel="bord" style="height:' + this.options.size + 'px;left:50%;background-color:' + this.options.color + '"></div><div class="bord_right" rel="bord" style="width:' + this.options.size + 'px;top:50%;height:0%;background-color:' + this.options.color + '"></div><div class="bord_bottom" rel="bord" style="height:' + this.options.size + 'px;right:50%;background-color:' + this.options.color + '"></div><div class="bord_left" rel="bord" style="width:' + this.options.size + 'px;bottom:50%;background-color:' + this.options.color + '"></div>';
+						}
 				else if( this.options.direction === true ){
 					var bord_top = '<div class="bord_top" rel="bord" style="width:100%;height:0px;left:0px;background-color:' + this.options.color + '"></div><div class="bord_right" rel="bord" style="width:0px;height:100%;top:0px;background-color:' + this.options.color + '"></div><div class="bord_bottom" rel="bord" style="height:0px;width:100%;right:0px;background-color:' + this.options.color + '"></div><div class="bord_left" rel="bord" style="width:0px;height:100%;bottom:0px;background-color:' + this.options.color + '"></div>';
 					}
         	break;
         	
 			case 'ackw':
-				if (this.options.direction === false){
+				if (this.options.direction === false && this.options.center === false){
 	        	var bord_top = '<div class="bord_top" rel="bord" style="height:' + this.options.size + 'px;right:0px;background-color:' + this.options.color + '"></div><div class="bord_right" rel="bord" style="width:' + this.options.size + 'px;bottom:0px;background-color:' + this.options.color + '"></div><div class="bord_bottom" rel="bord" style="height:' + this.options.size + 'px;left:0px;background-color:' + this.options.color + '"></div><div class="bord_left" rel="bord" style="width:' + this.options.size + 'px;top:0px;background-color:' + this.options.color + '"></div>';}
-	        	else if( this.options.direction === true ){
+				else if ((this.options.direction === false || this.options.direction === true) && this.options.center === true){
+		        	var bord_top = '<div class="bord_top" rel="bord" style="height:' + this.options.size + 'px;left:50%;background-color:' + this.options.color + '"></div><div class="bord_right" rel="bord" style="width:' + this.options.size + 'px;top:50%;height:0%;background-color:' + this.options.color + '"></div><div class="bord_bottom" rel="bord" style="height:' + this.options.size + 'px;right:50%;background-color:' + this.options.color + '"></div><div class="bord_left" rel="bord" style="width:' + this.options.size + 'px;bottom:50%;background-color:' + this.options.color + '"></div>';
+						}
+				else if( this.options.direction === true ){
 					var bord_top = '<div class="bord_top" rel="bord" style="width:100%;height:0px;left:0px;background-color:' + this.options.color + '"></div><div class="bord_right" rel="bord" style="width:0px;height:100%;top:0px;background-color:' + this.options.color + '"></div><div class="bord_bottom" rel="bord" style="height:0px;width:100%;right:0px;background-color:' + this.options.color + '"></div><div class="bord_left" rel="bord" style="width:0px;height:100%;bottom:0px;background-color:' + this.options.color + '"></div>';
 					}
 	        	break;
